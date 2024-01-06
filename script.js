fb2e41e7af34c3b4663af9d9d9209c05cc7b93fe
Anyona-00 <125492744+Anyona-00@users.noreply.github.com>
2024-01-06 21:04:15 +0300

    declaration

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
-/*const output = document.getElementById('output');
-let isError = false;*/
+const output = document.getElementById('output');
+let isError = false;
 
 /*function cleanInputString(str) {
   const strArray = str.split('');
